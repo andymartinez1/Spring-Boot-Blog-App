68c58863dcdf39decf5b86834ef3ef99c2aa8e06
Andrew Martinez <andy72391@gmail.com>
2024-12-15 01:02:18 -0500

    Added form validation. Added jQuery and CKEditor for the content field for custom blog post entries.

--- src/main/java/com/andymartinez1/blog_app/controller/PostController.java
+++ src/main/java/com/andymartinez1/blog_app/controller/PostController.java
@@ -2,9 +2,12 @@ package com.andymartinez1.blog_app.controller;
 
 import com.andymartinez1.blog_app.dto.PostDto;
 import com.andymartinez1.blog_app.service.PostService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -18,6 +21,13 @@ public class PostController {
         this.postService = postService;
     }
 
+    private static String getUrl(String postTitle) {
+        String title = postTitle.trim().toLowerCase();
+        String url = title.replaceAll("\\s+", "-");
+        url = url.replaceAll("[^a-zA-Z0-9-]", "-");
+        return url;
+    }
+
     @GetMapping("/admin/posts")
     public String getPosts(Model model) {
         List<PostDto> posts = postService.findAllPosts();
@@ -26,10 +36,23 @@ public class PostController {
     }
 
     @GetMapping("/admin/posts/newpost")
-    public String createPost(Model model) {
+    public String newPostForm(Model model) {
         PostDto postDto = new PostDto();
         model.addAttribute("post", postDto);
         return "/admin/create_post";
+    }
+
+    @PostMapping("/admin/posts")
+    public String createPost(@Valid @ModelAttribute("post") PostDto postDto,
+                             BindingResult result,
+                             Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("post", postDto);
+            return "/admin/create_post";
+        }
+        postDto.setUrl(postDto.getTitle());
+        postService.createPost(postDto);
+        return "redirect:/admin/posts";
     }
 
 }
--- src/main/java/com/andymartinez1/blog_app/dto/PostDto.java
+++ src/main/java/com/andymartinez1/blog_app/dto/PostDto.java
@@ -1,5 +1,6 @@
 package com.andymartinez1.blog_app.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +15,15 @@ import java.time.LocalDateTime;
 public class PostDto {
 
     private Long id;
+
+    @NotEmpty(message = "Title is required")
     private String title;
     private String url;
+
+    @NotEmpty(message = "Content is required")
     private String content;
+
+    @NotEmpty(message = "Short description is required")
     private String shortDescription;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
